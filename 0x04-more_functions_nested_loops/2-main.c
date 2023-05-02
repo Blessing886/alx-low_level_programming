f77abd9092cefd2dc250803ef02bf1cfe189c71a
Blessing886 <moraablessing082@gmail.com>
2023-05-02 04:39:38 -0700

    2-strlen.c

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -8,7 +9,11 @@
  */
 int main(void)
 {
-printf("%d\n", mul(98, 1024));
-printf("%d\n", mul(-402, 4096));
+char *str;
+int len;
+
+str = "My first strlen!";
+len = strlen(str);
+printf("%d\n", len);
 return (0);
 }
