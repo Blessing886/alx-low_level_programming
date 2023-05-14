0001254b0bb8aea29af935cacaee8f13e189cccd
Blessing886 <moraablessing082@gmail.com>
2023-05-14 06:58:45 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: the character to check
+ * Return: if c is lowercase 1, else 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
